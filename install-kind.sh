b89e92a8fec6a08f6d8c3a48da61a035e77aaff3
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-01-31 17:57:48 +0000

    refine

--- install-kind.sh
+++ install-kind.sh
@@ -3,7 +3,7 @@
 # Install kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+sudo mv ./kind /usr/local/bin/kind
 
 # Create a Kubernetes cluster
 kind create cluster --name k8s --config cluster.yaml
