c4b1b74c860b4cb3929c8b34569e0c23bd81a320
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-01-31 23:14:34 +0530

    Create install-kind.sh

---
+++ install-kind.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
+
+# Create a Kubernetes cluster
+kind create cluster --name k8s --config cluster.yaml
+
+# Set the KUBECONFIG environment variable
+export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
