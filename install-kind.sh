d9afbd3ff12b5a67364e37ffb5e1c4974d2c510d
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-04-09 17:13:05 +0000

    updated kind to 0.22

--- install-kind.sh
+++ install-kind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
